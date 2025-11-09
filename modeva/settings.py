21af64143fe63a5711cc8764e9452d83b6cfab6f
Tanvie Chopra <your_email@example.com>
2025-11-09 16:42:29 +0530

    Fix added Render domain to ALLOWED_HOSTS

--- modeva/settings.py
+++ modeva/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-wj^eizg*gtsqz_olrb85167%69w#$%_!ly^(jm(6gsnlmhismb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['modiva-fashion.onrender.com', '127.0.0.1', 'localhost']
+
 
 
 # Application definition
